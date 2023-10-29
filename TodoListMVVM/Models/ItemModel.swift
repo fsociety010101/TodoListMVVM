1bcb154890a5e41b3b35c13aae78605d75e1f2e1
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-29 13:33:31 +0100

    changed ItemModel
    ItemModel has two inits for adding new and updating/completing old items

--- TodoListMVVM/Models/ItemModel.swift
+++ TodoListMVVM/Models/ItemModel.swift
@@ -7,10 +7,31 @@
 
 import Foundation
 
+/* This struct is IMMUTABLE STRUCT */
+
 /* Identifiable to use inside a loop */
 
 struct ItemModel: Identifiable {
-    let id: String = UUID().uuidString
+    let id: String
     let title: String
     let isCompleted: Bool
+    
+    /*
+     
+     init id to be able to set id if needed, i have two constructors now
+     
+     USED TO ADD NEW ITEMS  - ItemModel(title: String, isCompleted: Bool)
+     USED TO UPDATE ITEMS   - ItemModel(id: String, title: String, isCompleted: Bool)
+     
+     */
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
 }
