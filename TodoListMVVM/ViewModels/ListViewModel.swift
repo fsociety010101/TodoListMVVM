315ed0c8d46c20fdccd1814293c0e858297c1082
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-30 14:24:31 +0100

    changed data to be persistent
    UserDefaults, JSON

--- TodoListMVVM/ViewModels/ListViewModel.swift
+++ TodoListMVVM/ViewModels/ListViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/* 
+/*
  
  CRUD FUNCTIONS
  
@@ -21,20 +21,34 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    // any time i change items array - didSet triggers
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second", isCompleted: true),
-            ItemModel(title: "Third", isCompleted: false)
-        ]
+//        let newItems = [
+//            ItemModel(title: "This is the first title", isCompleted: false),
+//            ItemModel(title: "This is the second", isCompleted: true),
+//            ItemModel(title: "Third", isCompleted: false)
+//        ]
+//        
+//        items.append(contentsOf: newItems)
         
-        items.append(contentsOf: newItems)
+        guard 
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -54,17 +68,23 @@ class ListViewModel: ObservableObject {
         
         /*
          
-        if let index = items.firstIndex { (existingItem) -> Bool in
-            return existingItem.id == item.id
-        } {
-            // run this code
-        }
+         if let index = items.firstIndex { (existingItem) -> Bool in
+         return existingItem.id == item.id
+         } {
+         // run this code
+         }
          
-        same as below!
-        */
+         same as below!
+         */
         
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
